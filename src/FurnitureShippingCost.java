68d683fe188fb0b887ae760a1dea005473cda749
VinceTedChua <vtmdomingochua@gmail.com>
2024-04-20 22:46:38 -0400

    Re-implementation of Visitor Pattern
    Did some tweaking within Visitor Pattern

--- src/FurnitureShippingCost.java
+++ src/FurnitureShippingCost.java
@@ -1,19 +1,18 @@
 class FurnitureShippingCost implements Shopper {
-    @Override
+
     public void shop(Chair chair) {
         int lightweight = 25;
         int shippingCost = lightweight * 200 / 10; // let weight be  25 and shipping per kg is 200
         System.out.println("The price shipping of the chair: PHP" + shippingCost);
     }
 
-    @Override
+
     public void shop(Table table) {
         int minVal = 10000;
         int shippingCost = minVal * 15 / 100; // Assuming a base price of 10000 for tables and 10% is deducted for shipping
         System.out.println("The price shipping of the table: PHP" + shippingCost);
     }
 
-    @Override
     public void shop(Sofa sofa) {
         int minVal = 600;
         int shippingCost = 2500 * 2; // Assuming a base price of 600 for sofas
